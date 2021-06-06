8bd15cd721c2bf0c4bdd5370f406eb9b61e49501
Varinder148 <34982959+Varinder148@users.noreply.github.com>
2021-06-06 15:58:58 +0530

    ui fixes

--- src/index.js
+++ src/index.js
@@ -1,28 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App'
-import './index.css';
-import {Switch} from 'react-router-dom'
-import {BrowserRouter, Route} from 'react-router-dom';
-import Homepage from './pages/homepage/homepage'
-import { Provider } from 'react-redux';
-import {store,persistor} from './redux/store'
-import { PersistGate, persistGate } from 'redux-persist/integration/react'
-
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import { Switch } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
+import Homepage from "./pages/homepage/homepage";
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
+import { PersistGate, persistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
   <Provider store={store}>
-  <BrowserRouter>
+    <HashRouter basename="/">
       <PersistGate persistor={persistor}>
-      <Switch>
-        <Route exact path='/homepage' component={Homepage} />  
-        <Route exact path='/' component={Homepage} />  
-        
-        <Route path='/' component={App} />
-        
-      </Switch>
+        <Switch>
+          <Route exact path="/homepage" component={Homepage} />
+          <Route exact path="/" component={Homepage} />
+          <Route path="/" component={App} />
+        </Switch>
       </PersistGate>
-  </BrowserRouter>
+    </HashRouter>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
